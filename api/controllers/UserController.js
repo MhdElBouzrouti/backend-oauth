ce707cc80c4501ce3f195dda29547436b3a91d33
Mohammed EL BOUZROUTI <Mohammedelbouzrouti@gmail.com>
2016-04-24 20:59:34 +0200

    Initialization of project

---
+++ api/controllers/UserController.js
@@ -0,0 +1,72 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+  login: function (req, res) {
+    var accessTokenUrl = 'https://api.orange.com/openidconnect/fr/v1/token';
+    var userInfoUrl = 'https://api.orange.com/openidconnect/v1/userinfo';
+    var params = {
+      code: req.body.code,
+      client_id: req.body.clientId,
+      client_secret: config.GOOGLE_SECRET,
+      redirect_uri: req.body.redirectUri,
+      grant_type: 'authorization_code'
+    };
+
+    // Step 1. Exchange authorization code for access token.
+    request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
+      var accessToken = token.access_token;
+      var headers = { Authorization: 'Bearer ' + accessToken };
+
+      // Step 2. Retrieve profile information about the current user.
+      request.get({ url: userInfoUrl, headers: headers, json: true }, function(err, response, profile) {
+        if (profile.error) {
+          return res.status(500).send({message: profile.error.message});
+        }
+        // Step 3a. Link user accounts.
+        if (req.header('Authorization')) {
+          User.findOne({ google: profile.sub }, function(err, existingUser) {
+            if (existingUser) {
+              return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
+            }
+            var token = req.header('Authorization').split(' ')[1];
+            var payload = jwt.decode(token, config.TOKEN_SECRET);
+            User.findById(payload.sub, function(err, user) {
+              if (!user) {
+                return res.status(400).send({ message: 'User not found' });
+              }
+              user.google = profile.sub;
+              user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
+              user.displayName = user.displayName || profile.name;
+              user.save(function() {
+                var token = createJWT(user);
+                res.send({ token: token });
+              });
+            });
+          });
+        } else {
+          // Step 3b. Create a new user account or return an existing one.
+          User.findOne({ google: profile.sub }, function(err, existingUser) {
+            if (existingUser) {
+              return res.send({ token: createJWT(existingUser) });
+            }
+            var user = new User();
+            user.google = profile.sub;
+            user.picture = profile.picture.replace('sz=50', 'sz=200');
+            user.displayName = profile.name;
+            user.save(function(err) {
+              var token = createJWT(user);
+              res.send({ token: token });
+            });
+          });
+        }
+      });
+    });
+  }
+
+};
+
